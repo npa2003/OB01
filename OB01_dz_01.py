73b8694de9b4a383014da0d8245464c44aa4a012
Pavel N <npa2003@gmail.com>
2025-01-17 01:43:42 +0300

    Update OB01_dz_01.py

--- OB01_dz_01.py
+++ OB01_dz_01.py
@@ -6,31 +6,33 @@ class Tasks():
         self.task.append([name, dt, False])
         print(f'Задача с именем: {name}   Добавлена.')
 
-    def mark_task(self, name):
-        pass
+    def mark_task(self, name):  # отмечаем задание по имени
+        for i in range(len(self.task)):
+            if self.task[i][0] == name:
+                self.task[i][2] = True
 
-    def info_task(self): # вывод всех задач
+    def info_task(self):  # вывод всех задач
         for i in range(len(self.task)):
             marked = 'НЕ выполнена'
             if self.task[i][2]:
                 marked = 'Выполнена'
-            print(f'{i+1}.   {self.task[i][0]}   {self.task[i][1]}   {marked}')
+            print(f'{i + 1}.   {self.task[i][0]}   {self.task[i][1]}   {marked}')
 
-    def info_task_marked(self): # вывод отмеченных задач
+    def info_task_marked(self):  # вывод отмеченных задач
         for i in range(len(self.task)):
             marked = 'НЕ выполнена'
             if self.task[i][2]:
                 marked = 'Выполнена'
-                print(f'{i+1}.   {self.task[i][0]}   {self.task[i][1]}   {marked}')
+                print(f'{i + 1}.   {self.task[i][0]}   {self.task[i][1]}   {marked}')
 
-    def info_task_unmarked(self): # вывод НЕотмеченных задач
+    def info_task_unmarked(self):  # вывод НЕотмеченных задач
         for i in range(len(self.task)):
             marked = 'НЕ выполнена'
             if not self.task[i][2]:
-                print(f'{i+1}.   {self.task[i][0]}   {self.task[i][1]}   {marked}')
+                print(f'{i + 1}.   {self.task[i][0]}   {self.task[i][1]}   {marked}')
 
 
-#__________________________________________________________________________________________
+# __________________________________________________________________________________________
 
 def com_list():
     print('\n/list - вывести список невыполненных задач')
@@ -42,7 +44,7 @@ def com_list():
     print('ЛЮБОЙ другой набор сиволов - вывести список команд\n')
 
 
-#__________________________________________________________________________________________
+# __________________________________________________________________________________________
 tb = Tasks()
 tb.add_task('Задача-1', '21-01-2025')
 tb.add_task('Задача-2', '21-01-2025')
@@ -51,8 +53,8 @@ com_list()
 
 while True:
     c = input('\nВведите команду: ')
-    split_c = c.rsplit() # разделяем на команду и параметры
-    com = split_c[0] # вынимаем команду
+    split_c = c.rsplit()  # разделяем на команду и параметры
+    com = split_c[0]  # вынимаем команду
     match com:
         case '/list':
             tb.info_task_unmarked()
@@ -64,8 +66,7 @@ while True:
             tb.info_task()
 
         case '/add':
-            tb.add_task(split_c[1],split_c[2])
-            # print('Пока не обрабатываем')
+            tb.add_task(split_c[1], split_c[2])  # print('Пока не обрабатываем')
 
         case '/mark':
             print('Пока не обрабатываем')
@@ -74,6 +75,7 @@ while True:
             print('До свидания!')
             exit(0)
 
-        case _: com_list()
+        case _:
+            com_list()
 
 tb.info_task()
