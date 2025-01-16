075893ccc3c8251de568e837a3c34613cfcca1d8
Pavel N <npa2003@gmail.com>
2025-01-17 00:39:24 +0300

    Update OB01_dz_01.py

--- OB01_dz_01.py
+++ OB01_dz_01.py
@@ -13,11 +13,15 @@ class Tasks():
         for i in range(len(self.task)):
             print(i)
             print(self.task[i][0])
-            
+
             # print(i[0]) # print(i[1]) # print(i[2])
 
 
 tb = Tasks()
+
+while True:
+    com = input()
+
 tb.add_task('Задача-1', '21-01-2025')
 tb.add_task('Задача-2', '21-01-2025')
 tb.info_task()
